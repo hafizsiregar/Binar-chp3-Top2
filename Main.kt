ece31192e4e8f452df7406f187b3411250bbc6ae
Muhammad Hafiz Siregar <72025965+hafizsiregar@users.noreply.github.com>
2022-03-15 19:03:17 +0700

    tugas binar

---
+++ Main.kt
@@ -0,0 +1,49 @@
+package and.binar.bangundatarand
+
+import java.lang.Exception
+
+fun main() = try {
+    print(
+        "Masukan Jenis Bangun Datar - Persegi(a), Persegi Panjang(b), Segitiga(c), " +
+                "Lingkaran(d), Jajar Genjang(e), Belah Ketupat(f), Trapesium(g), Layang Layang(h):"
+    ) //update the master
+    when (readLine()) {
+        "a" -> {
+            //Hitung Persegi
+        }
+
+        "b" -> {
+            //Hitung Persegi Panjang
+        }
+
+        "c" -> {
+            //Hitung Segitiga
+        }
+
+        "d" -> {
+            //Hitung Lingkaran
+        }
+
+        "e" -> {
+            //Hitung Jajar Genjang
+        }
+
+        "f" -> {
+            //Hitung Belah Ketupat
+        }
+
+        "g" -> {
+            //Hitung Trapesium
+        }
+
+        "h" -> {
+            //Hitung Layang Layang
+        }
+
+        else -> {
+            print("Operasi Tidak Valid")
+        }
+    }
+} catch (i: Exception){
+    print("Pilih Jenis Bangun Datar")
+}
